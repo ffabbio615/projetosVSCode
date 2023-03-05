dee9df2a378b79d50bc9dbbecc770e86d9173946
ffabbio615 <fabiomarques.ads@gmail.com>
2023-03-05 20:20:26 -0300

    Adição do exercício com MATH

---
+++ maths.js
@@ -0,0 +1,27 @@
+let numerosDoSorteio = [];
+let numeroSorteado;
+for (let i=0; i<10; i++)
+{
+    console.log (numeroSorteado = Math.floor(Math.random() * 10 +1));
+    for (let j=0; j<10; j++)
+    {
+    if (numerosDoSorteio[j]!=undefined)
+    {
+        if(numeroSorteado==numerosDoSorteio[j])
+        {
+            j=10;
+            i--;
+        }
+    }
+    else if(numerosDoSorteio[i]==undefined && j!=10){
+        numerosDoSorteio[i] = numeroSorteado;
+    }
+}
+}
+for (let i=0; i<10; i++){
+    console.log("Número "+ (i+1) + ": " + numerosDoSorteio[i]);
+}
+
+let testandoCeil = 7.12;
+
+console.log(Math.ceil(testandoCeil));
