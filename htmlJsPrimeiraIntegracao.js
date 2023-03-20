cb6f19650310237c0181b7e25fc1cf7c253dbaa9
ffabbio615 <fabiomarques.ads@gmail.com>
2023-03-20 20:58:12 -0300

    Funções

--- htmlJsPrimeiraIntegracao.js
+++ htmlJsPrimeiraIntegracao.js
@@ -1,6 +1,14 @@
-const numero = prompt('Digite um Número:');
+let numero;
+
+function informarNumero(n)
+{
 const numeroTitulo = document.getElementById('numeroTitulo');
 const texto = document.getElementById('texto');
 
-numeroTitulo.innerHTML = numero;
-numeroTitulo.outerHTML = numero;
+numeroTitulo.innerHTML = n;
+texto.innerHTML = `<p>Seu número + 2 é: ${Number(n) + 2}. </p>`;
+return alert (n);
+}
+
+numero = informarNumero(prompt('Digite um Número:'));
+
