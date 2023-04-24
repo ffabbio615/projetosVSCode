bc21a1aaeba36e7c4c0dd4a9703851db186a3ed5
ffabbio615 <fabiomarques.ads@gmail.com>
2023-04-23 21:26:06 -0300

    Atualização da página da Sunset

---
+++ SunsetRioRental/assets/js/main.js
@@ -0,0 +1,5 @@
+const something = document.getElementById('trApartamentos');
+something.onclick = function() {
+    let botaoMenu = document.querySelector("#botaoMenu");
+    botaoMenu.className = "botaoMenuVisivel";
+};
