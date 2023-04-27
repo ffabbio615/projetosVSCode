0be9b2c952c33e413367c403534898dc3f3ad1e8
ffabbio615 <fabiomarques.ads@gmail.com>
2023-04-27 17:26:06 -0300

    sal

---
+++ SunsetRioRental/assets/js/traducao.js
@@ -0,0 +1,32 @@
+// ID's de Tradução
+// trTitulo = Sunset Rio – Imoveis de temporada em Copacabana
+// trHorarioTopo = Seg - Sáb: 9:00 - 18:00
+// trApartamentos = APARTAMENTOS
+// trPagamentos = POLÍTICAS
+// trSobreNos = SOBRE NÓS
+// trTituloSecao1 = Você já contemplou a vida hoje? Com a gente, isso é possível!
+// trParagrafoSecao1 =  Apartamentos limpos, com vista para o mar, a 100m da praia, próximo aos transportes mais utilizados e totalmente equipado para que sua experiência seja incrível.
+// trBotaoCTA = Reserve Agora
+
+// SEGUNDA PÁGINA
+
+// trTituloApartamentos = NOSSOS APARTAMENTOS
+// trParagrafoApartamentos = Confira a seguir o que temos de melhor para sua estadia:
+trBotaoDetalhes1 = DETALHES
+id="trC1.1"
+id="trC1.2"
+id="trC1.3"
+trBotaoDetalhes2 = DETALHES
+trBotaoDetalhes3 = DETALHES
+trBotaoDetalhes4 = DETALHES
+trBotaoDetalhes5 = DETALHES
+trBotaoDetalhes6 = DETALHES
+trBotaoDetalhes7 = DETALHES
+trBotaoDetalhes8 = DETALHES
+trBotaoDetalhes9 = DETALHES
+trBotaoDetalhes10 = DETALHES
+trBotaoDetalhes11 = DETALHES
+trBotaoDetalhes12 = DETALHES
+
+trTituloAvaliacoes = AVALIAÇÕES DE HÓSPEDES
+trParagrafoAvaliacoes = Confira a seguir o que nossos hóspedes acharam da experiência:
