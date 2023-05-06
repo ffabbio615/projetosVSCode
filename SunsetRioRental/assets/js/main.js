e7f4de7a09d5dd9b0f7a6dc940abb458a09f1c8b
ffabbio615 <fabiomarques.ads@gmail.com>
2023-05-05 21:52:21 -0300

    50% da janela modal concluído

--- SunsetRioRental/assets/js/main.js
+++ SunsetRioRental/assets/js/main.js
@@ -3,9 +3,22 @@
 //     let botaoMenu = document.querySelector("#botaoMenu");
 //     botaoMenu.className = "botaoMenuVisivel botaoMenu";
 // };
+let borrar = document.querySelector("#trBotaoCTA");
+// const linkApartamento = document.location.href;
+// if (linkApartamento == "http://25.10.190.140:5500/SunsetRioRental/index.html#modalApartamento" || linkApartamento == "http://127.0.0.1:5500/SunsetRioRental/index.html#modalApartamento")
+// {
+//     // alert("O JavaScript pega o link e checa apenas uma vez, daí se o link for esse especial, vai abrir uma modal com as informações");
+//     borrar = document.querySelector("#secaoInicial")
+//     borrar.className = "secaoInicial blurModal"
+// }
 
-const linkApartamento = document.location.href;
-if (linkApartamento == "http://25.10.190.140:5500/SunsetRioRental/index.html#secaoSobreNos")
-{
-    alert("O JavaScript pega o link e checa apenas uma vez, daí se o link for esse especial, vai abrir uma modal com as informações");
-}
+const fecharModal = document.querySelector("#btnFecharModalApartamento");
+borrar.onclick = function(){
+    borrar = document.querySelector("#secaoInicial")
+    borrar.className = "secaoInicial blurModal";
+};
+
+fecharModal.onclick = function(){
+    borrar = document.querySelector("#secaoInicial")
+    borrar.className = "secaoInicial";
+};
