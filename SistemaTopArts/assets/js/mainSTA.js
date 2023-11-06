2edd8a78b7bddaeaea72d388b4ba1952f7754c58
ffabbio615 <fabiomarques.ads@gmail.com>
2023-11-06 18:56:31 -0300

    Atualização da página STA com os links abrindo dinamicamente com fetch api dentro da div principal

--- SistemaTopArts/assets/js/mainSTA.js
+++ SistemaTopArts/assets/js/mainSTA.js
@@ -13,8 +13,19 @@ async function loadPage(link){
         const response = await fetch(link);
         if (response.status !== 200) throw new Error('Esta página não pôde ser carregada.');
         const page = await response.text();
+        changeTittle(link);
         openPage(page);
     } catch (error){alert(error);}
+}
+
+function changeTittle(link){
+    const headerTitle = document.querySelector('#header-title');
+    if(link.includes('ultimosEnvios')) headerTitle.innerText = 'Últimos Envios dos Sublimadores';
+    if(link.includes('perfil')) headerTitle.innerText = 'Meu Perfil';
+    if(link.includes('minhasMoedas')) headerTitle.innerText = 'Minhas Moedas';
+    if(link.includes('adquirirArtes')) headerTitle.innerText = 'Adquirir Artes';
+    if(link.includes('recompensas')) headerTitle.innerText = 'Minhas Recompensas';
+    if(link.includes('minhasArtes')) headerTitle.innerText = 'Minhas Artes';
 }
 
 function openPage(page){
