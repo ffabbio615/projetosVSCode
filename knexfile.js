a91adc62eca60f8bf938d69ddf5912b24ac90c5b
ffabbio615 <fabiomarques.ads@gmail.com>
2024-02-15 09:33:17 -0300

    Adição do Knex e criação da tabela users no DB

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    client: 'mysql2',
+    connection: {
+      database: 'base_de_dados_knex',
+      user:     'root',
+      password: 'admin'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  };
