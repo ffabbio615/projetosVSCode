f0baa70bae39807242b75697d7284fb4e9293e0c
ffabbio615 <fabiomarques.ads@gmail.com>
2024-10-19 20:58:59 -0300

    Adição do Projeto Prime Language School em REACT

---
+++ Projetos Pessoais/PrimeLanguageSchool/PrimeLanguageSchool/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.scss'
+import Header from './components/Header';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header />
+
+
+    </>
+  )
+}
+
+export default App
