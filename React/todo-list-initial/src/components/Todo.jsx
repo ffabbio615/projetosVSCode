513eaa234fe3cf32467fcff37b18ff889cfa706c
ffabbio615 <126684842+ffabbio615@users.noreply.github.com>
2024-11-29 20:35:57 -0300

    Adição de código da aula sobre context nas anotações. Falta agora adicionar no projeto em si

--- React/todo-list-initial/src/components/Todo.jsx
+++ React/todo-list-initial/src/components/Todo.jsx
@@ -1,20 +1,20 @@
 import './Todo.scss'
-function Todo({ title, description, isDone }) {
+function Todo({ todo, removeTodo, marcaTodo }) {
 
   return (
     <>
-        <div className={`todo ${isDone ? 'done' : ''}`}>
-            <button className="erase">x erase</button>
+        <div className={`todo ${todo.isDone ? 'done' : ''}`}>
+            <button className="erase" onClick={() => removeTodo(todo.id)}>x erase</button>
             <h3>
-                {title}
+                {todo.title}
             </h3>
             <p>
-                {description}
+                {todo.description}
             </p>
             <div className="task-check">
-                <input type="checkbox" checked={isDone} />
+                <input type="checkbox" onClick={() => marcaTodo(todo.id)} defaultChecked={todo.isDone} />
                 <label>
-                    {!isDone ? 'To-Do' : 'Done'}
+                    {!todo.isDone ? 'To-Do' : 'Done'}
                 </label>
             </div>
         </div>
--- React/todo-list-initial/src/components/TodosList.jsx
+++ React/todo-list-initial/src/components/TodosList.jsx
@@ -1,22 +1,47 @@
+import React, {useState} from 'react';
 import Todo from './Todo.jsx';
 
+const lista = [
+  { id: 0, title: 'Do Groceries', description: 'Buy apples, rice, juice and toilet paper.', isDone: true },
+  { id: 1, title: 'Study React', description: 'Understand context & reducers.', isDone: false},
+  { id: 2, title: 'Learn Redux', description: 'Learn state management with Redux', isDone: false }
+];
+
+
 function TodosList() {
+
+  const [listar, setListar] = useState(lista);
+
+  function removerTodo(id)
+  {
+    if(confirm("Tem certeza que deseja remover essa tarefa da lista?")){
+      setListar(listar.filter(todo => todo.id !== id));
+    }
+  }
+
+  function marcarTodo(id){
+    setListar(listar.map(todo =>{
+      if (todo.id === id){
+        todo.isDone = !todo.isDone;
+        return todo;
+      }
+      else{
+        return todo;
+      }
+    }))}
 
   return (
     <>
         <div className="todos">
-
+          {listar.map(todo =>
             <Todo
-              title="Do Groceries"
-              description="Buy apples, rice, juice and toilet paper."
-              isDone={true}
+            key={todo.id}
+            todo={todo}
+            removeTodo={(id) => removerTodo(id)}
+            marcaTodo={(id) => marcarTodo(id)}
             />
-
-            <Todo
-              title="Study React"
-              description="Understand context, reducers and state management with Redux."  
-              isDone={false}
-            />
+          )
+        }
 
         </div>
     </>
