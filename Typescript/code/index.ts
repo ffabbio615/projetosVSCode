6cc5f6e8f45b3cb004f4d25c1261e1d0b80cd0b7
ffabbio615 <126684842+ffabbio615@users.noreply.github.com>
2025-03-02 02:57:02 -0300

    Adição de anotações do curso de TypeScript dia2

--- Typescript/code/index.ts
+++ Typescript/code/index.ts
@@ -3,4 +3,39 @@
 let a = 10;
 let b = 'Andre';
 
+const array: number[] = [];
+array.push(10,20,30,40);
+
 console.log(`Hello, ${b}! Your score today is ${a}! Come back tomorrow to increase it!`);
+console.log(array[2]);
+
+// let item: [string, number, boolean] = ['Apple', 3, true];
+
+// const item: [string, number, boolean] = [];
+
+function soma (n1: number, n2: number){ 
+    if(n1 + n2 >= 10){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+console.log(soma(5,6));
+
+
+// const user:{
+//     firstName: string,
+//     age: number,
+//     lastName: string,
+// } = {
+//     firstName: "Fábio",
+//     age: 33,
+//     lastName: "Marques Melo"
+// }
+
+const user:{
+    firstName?: string,
+    age?: number,
+    lastName?: string,
+} = {}
--- Typescript/js/index.js
+++ Typescript/js/index.js
@@ -1,4 +1,28 @@
 "use strict";
 let a = 10;
 let b = 'Andre';
+const array = [];
+array.push(10, 20, 30, 40);
 console.log(`Hello, ${b}! Your score today is ${a}! Come back tomorrow to increase it!`);
+console.log(array[2]);
+// let item: [string, number, boolean] = ['Apple', 3, true];
+// const item: [string, number, boolean] = [];
+function soma(n1, n2) {
+    if (n1 + n2 >= 10) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(soma(5, 6));
+// const user:{
+//     firstName: string,
+//     age: number,
+//     lastName: string,
+// } = {
+//     firstName: "Fábio",
+//     age: 33,
+//     lastName: "Marques Melo"
+// }
+const user = {};
